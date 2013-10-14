723c17ba48414af20e45f234b7b26515f2fe127a
ibaquez <ibaquez@gmail.com>
2013-10-14 01:47:42 -0300

    llo

--- Obra/src/com/catinabox/obra/LogInProcessActivity.java
+++ Obra/src/com/catinabox/obra/LogInProcessActivity.java
@@ -14,7 +14,7 @@ public class LogInProcessActivity extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present. kk
+		// Inflate the menu; this adds items to the action bar if it is present. kktt
 		getMenuInflater().inflate(R.menu.log_in_process, menu);
 		return true;
 	}
